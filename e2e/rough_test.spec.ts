f32ab588b6b19d45c486b9ddf8d1525bfec1e575
Kunal-kankariya <kunalkankriya2203@gmail.com>
2025-04-25 11:51:43 +0530

    [Ticketid]: Test to select product and checkout

--- e2e/rough_test.spec.ts
+++ e2e/rough_test.spec.ts
@@ -1,12 +1,46 @@
-import{expect, test} from '@playwright/test'
+import { expect, test } from '@playwright/test';
+import { login } from '../utils/common';
 
-test('Valid login', async({page})=>{
-  await page.goto('https://www.saucedemo.com/')
-  await expect(page).toHaveURL('https://www.saucedemo.com/')
-  await expect(page).toHaveTitle('Swag Labs')
-  await page.getByPlaceholder('Username').fill('standard_user')
-  await page.getByPlaceholder('Password').fill('secret_sauce')
-  await page.locator('[data-test="login-button"]').click()
-  await expect(page.locator('[data-test="inventory-list"]')).toBeVisible()
+test.describe('UI validation', () => {
+  test.beforeEach(async ({ page }) => {
+    await login(page); // 🔐 Login before each test
+  });
+
+  test('Valid login', async ({ page }) => {
+    const btnText = await page.locator('div[data-test="inventory-item"] button').first().textContent()
+    const discriptionText = await page.locator('div[data-test="inventory-item-description"]  a').first().textContent() as string
+    const itemPriceText = await page.locator('div[data-test="inventory-item"] [data-test="inventory-item-price"]').first().textContent()  as string
+    console.log('itemPriceText', itemPriceText); // ✅ Simple test case
+    console.log('btnText', btnText); // ✅ Simple test case
+    console.log('discriptionText', discriptionText); // ✅ Simple test case
+    expect(btnText).toBe('Add to cart')
+    await page.locator('div[data-test="inventory-item"] button').first().click();
+    const AddedCartBtnText = await page.locator('div[data-test="inventory-item"] button').first().textContent()
+    console.log('AddedCartBtnText', AddedCartBtnText); // ✅ Simple test case
+    expect(AddedCartBtnText).toBe('Remove')
+    await page.locator('[data-test="shopping-cart-link"]').click()
+    await expect(page).toHaveURL('https://www.saucedemo.com/cart.html')
+    await expect(page.locator('[data-test="cart-list"]')).toHaveCount(1)
+    await expect(page.locator('[data-test="cart-list"]')).toContainText(discriptionText)
+    await expect(page.locator('[data-test="cart-list"]')).toContainText(itemPriceText)
+    await page.getByRole('button', {name: 'Checkout'}).click()
+    await expect(page.getByText('Checkout: Your Information')).toBeVisible()
+    await page.getByPlaceholder('First Name').fill('john')
+    await page.getByPlaceholder('Last Name').fill('doe')
+    await page.getByPlaceholder('Zip/Postal Code').fill('123456')
+    await page.locator('[data-test="continue"]').click()
+    await expect(page.getByText(discriptionText)).toBeVisible()
+    await expect(page.getByText(itemPriceText).first()).toBeVisible()
+    await page.getByRole('button', {name: 'finish'}).click()
+    await expect(page.getByText('Thank you for your order!')).toBeVisible()
+    await page.getByRole('button', {name: 'Back Home'}).click()
+    await expect(page.locator('[data-test="inventory-list"]')).toBeVisible()
+  });
   
-})
+  test.fixme('Valid login new', async ({ page }) => {
+    console.log('fix it'); // ✅ Simple test case
+  });
+
+
+});
+
